411b6c12de364a49866e8ddd1389b9ce8cfb9d7f
Abdelmounaim Elharba <monimeelharba123@gmail.com>
2025-06-05 17:11:57 +0100

    d

--- messaging_app/chats/views.py
+++ messaging_app/chats/views.py
@@ -1,3 +1,45 @@
-from django.shortcuts import render
+from rest_framework import viewsets, permissions
+from rest_framework.response import Response
+from rest_framework.decorators import action
+from django.contrib.auth import get_user_model
+from .models import Conversation, Message
+from .serializers import ConversationSerializer, MessageSerializer
 
-# Create your views here.
+User = get_user_model()
+
+class ConversationViewSet(viewsets.ModelViewSet):
+    queryset = Conversation.objects.all()
+    serializer_class = ConversationSerializer
+    permission_classes = [permissions.AllowAny] 
+
+    def perform_create(self, serializer):
+       
+        serializer.save()
+
+    @action(detail=True, methods=['post'])
+    def send_message(self, request, pk=None):
+        """إرسال رسالة داخل محادثة محددة"""
+        conversation = self.get_object()
+        sender = request.user 
+        content = request.data.get('content')
+
+        if not content:
+            return Response({"error": "Content is required."}, status=400)
+
+        message = Message.objects.create(
+            conversation=conversation,
+            sender=sender,
+            content=content
+        )
+        serializer = MessageSerializer(message)
+        return Response(serializer.data, status=201)
+
+
+class MessageViewSet(viewsets.ModelViewSet):
+    queryset = Message.objects.all()
+    serializer_class = MessageSerializer
+    permission_classes = [permissions.AllowAny] 
+
+    def perform_create(self, serializer):
+       
+        serializer.save(sender=self.request.user)
