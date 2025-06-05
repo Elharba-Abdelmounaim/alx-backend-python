4801e2c3b44f3ee7ae9b26bc9d928d2973dd70cf
Abdelmounaim Elharba <monimeelharba123@gmail.com>
2025-06-05 16:54:24 +0100

    s

--- messaging_app/chats/models.py
+++ messaging_app/chats/models.py
@@ -1,12 +1,11 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
 # 1. User model extending AbstractUser
-
 class User(AbstractUser):
     bio = models.TextField(blank=True, null=True)
 
+# 2. Conversation model
 class Conversation(models.Model):
     participants = models.ManyToManyField(User, related_name='conversations')
     created_at = models.DateTimeField(auto_now_add=True)
@@ -14,12 +13,13 @@ class Conversation(models.Model):
     def __str__(self):
         return f"Conversation {self.id} with {self.participants.count()} participants"
 
+# 3. Message model
 class Message(models.Model):
     conversation = models.ForeignKey(Conversation, related_name='messages', on_delete=models.CASCADE)
     sender = models.ForeignKey(User, related_name='sent_messages', on_delete=models.CASCADE)
     content = models.TextField()
     timestamp = models.DateTimeField(auto_now_add=True)
-    is_read = models.BooleanField(default=False)  
+    is_read = models.BooleanField(default=False)
 
     def __str__(self):
         return f"Message from {self.sender.username} at {self.timestamp}"
