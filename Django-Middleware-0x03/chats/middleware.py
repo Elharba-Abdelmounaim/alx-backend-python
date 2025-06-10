19e70f303e5e542a25788328835893aae7116cbc
Abdelmounaim Elharba <monimeelharba123@gmail.com>
2025-06-10 21:38:42 +0100

    middlewere

---
+++ Django-Middleware-0x03/chats/middleware.py
@@ -0,0 +1,17 @@
+import logging
+from datetime import datetime
+
+class RequestLoggingMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+        self.logger = logging.getLogger('request_logger')
+        handler = logging.FileHandler('chats/requests.log')
+        formatter = logging.Formatter('%(message)s')
+        handler.setFormatter(formatter)
+        self.logger.addHandler(handler)
+        self.logger.setLevel(logging.INFO)
+
+    def __call__(self, request):
+        user = request.user if request.user.is_authenticated else 'AnonymousUser'
+        self.logger.info(f"{datetime.now()} - User: {user} - Path: {request.path}")
+        return self.get_response(request)
--- Django-Middleware-0x03/chats/tests.py
+++ Django-Middleware-0x03/chats/tests.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
+from django.contrib.auth import get_user_model
 
-# Create your tests here.
+class CustomUserModelTests(TestCase):
+    def test_create_user_with_bio(self):
+        User = get_user_model()
+        user = User.objects.create_user(username='testuser', password='pass1234', bio='Hello bio!')
+        self.assertEqual(user.bio, 'Hello bio!')
+        self.assertTrue(user.check_password('pass1234'))
